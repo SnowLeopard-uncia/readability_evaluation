6d9818d7efcd3ccf0bc0b5523786091e5e1fa494
SnowLeopard-uncia <1976150655@qq.com>
2021-10-28 16:21:54 +0800

    update the part of ChooseLanguage

--- app/src/main/java/com/example/bamboo/UI/AudioActivity.java
+++ app/src/main/java/com/example/bamboo/UI/AudioActivity.java
@@ -1,16 +1,25 @@
 package com.example.bamboo.UI;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.Build;
 import android.os.Bundle;
 
 import com.example.bamboo.R;
 
-public class AudioActivity extends AppCompatActivity {
+public class AudioActivity extends BaseActivity {
 
+    @RequiresApi(api = Build.VERSION_CODES.M)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_audio);
+        init();
     }
+
+    private void init() {
+        initNavBar(true,"");
+    }
+
 }
